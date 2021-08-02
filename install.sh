78bbf6bc77c2f67910ea70240f9ed4be0430d653
Mario Voigt <mario.voigt@stadtfabrikanten.org>
2021-08-02 18:10:13 +0200

    fixed scad thumb generator

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@
 #install openscad
 sudo dnf install openscad
 
+#copy thumbnail generators
 sudo cp -rf stl_thumb.py /usr/bin/
 sudo chmod a+x /usr/bin/stl_thumb.py
+sudo cp -rf scad_thumb.py /usr/bin/
+sudo chmod a+x /usr/bin/scad_thumb.py
+
+#copy executors
 sudo cp -rf stl.thumbnailer /usr/share/thumbnailers/
-sudo cp -rf openscad.thumbnailer /usr/share/thumbnailers/
+sudo cp -rf scad.thumbnailer /usr/share/thumbnailers/
 
 #copy mime files. By installing OpenSCAD, an openscad.xml is automatically put in the correct target path
 sudo cp -rf stl.xml /usr/share/mime/packages/
+sudo cp -rf scad.xml /usr/share/mime/packages/
 
 #update mime database
 sudo update-mime-database /usr/share/mime/
---
+++ scad_thumb.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+import hashlib
+import shutil
+
+fin = ""
+fout = ""
+size = ""
+
+def main():
+    m = hashlib.md5()
+    m.update(fin)
+
+    ff = "/tmp/scad_to_png_%s.scad" % m.hexdigest()
+
+    m = hashlib.md5()
+    m.update(fin)
+
+    try:
+        shutil.copyfile(fin.decode("UTF-8"), ff)
+    except Exception as e:
+        print("Copy error")
+ 
+    cmd = "openscad -o %s.png --imgsize=%s,%s %s 2> /dev/null; mv %s.png %s" % (fout, size, size, ff, fout, fout)
+    os.system(cmd)
+    #os.remove(ff)
+    sys.exit(0)
+
+if __name__ == '__main__':
+
+    #log file - enable to debug
+    f = open("/tmp/scad_to_png_tmp.log", "w")
+    f.write("%i %s" % (len(sys.argv), sys.argv))
+    f.close()
+
+    if len(sys.argv) != 4:
+        print("add args [in file] [out file] [size]")
+        sys.exit(0)
+    else:
+        fin = sys.argv[1].encode("UTF-8")
+        fout = sys.argv[2]
+        size = sys.argv[3]
+
+    main()
